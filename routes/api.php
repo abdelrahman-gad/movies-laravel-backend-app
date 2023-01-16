e54434b5bf3a3f4d14a7549d9d37f22ac9c8b4b9
abdelrahman-gad <abdelrahmangad95@gmail.com>
2023-01-16 23:46:35 +0200

    remove some routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-// Route::resource('products', ProductController::class);
-
 // Public routes
 Route::post('/login', [AuthController::class, 'login']);
 
